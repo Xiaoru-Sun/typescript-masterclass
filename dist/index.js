30e0183ad5cb8eccfe2d3075688f3a9a1e9ac804
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-13 10:47:55 +0100

    typescript functions

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,20 @@
-console.log("compiled");
+"use strict";
+/* functions */
+function addNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumbers = (a, b) => {
+    return a - b;
+};
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+    // return total;
+}
+addAllNumbers([1, 2, 4, 5]);
+// return type inference //
+const formatGreeting = (name, greeting) => {
+    return `${greeting} ${name}`;
+};
+const result = formatGreeting("marios", "hello");
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 /* functions */
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const subtractTwoNumbers = (a: number, b: number): number => {
+  return a - b;
+};
+
+function addAllNumbers(items: number[]): void {
+  const total = items.reduce((a, c) => a + c, 0);
+  console.log(total);
+  // return total;
+}
+addAllNumbers([1, 2, 4, 5]);
+
+// return type inference //
+
+const formatGreeting = (name: string, greeting: string): string => {
+  return `${greeting} ${name}`;
+};
+
+const result = formatGreeting("marios", "hello");
+console.log(result);
