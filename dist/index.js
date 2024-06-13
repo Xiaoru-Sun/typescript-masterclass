fdecf493d3bc8ea49d79355de841f15840a62304
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-13 12:30:10 +0100

    typescript tuples, adds an example of REST paparameters

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,19 @@
 //--------
 // tuples
 //--------
-let person = ['mario', 30, true];
+let person = ["mario", 30, true];
 // person[0] = false
-person[0] = 'mario';
+person[0] = "luigi";
+console.log(person[0]);
 //-----------------
 // tuples examples
 //-----------------
 let hsla;
-hsla = [200, '100%', '50%', 1];
+hsla = [200, "100%", "50%", 1];
 let xy;
 xy = [94.7, 20.1];
+let tup;
+tup = ["Alex", 5];
 function useCoords() {
     // get coords of something
     const lat = 100;
@@ -19,9 +22,22 @@ function useCoords() {
     return [lat, long];
 }
 const [lat, long] = useCoords();
+const arsenal = ["Man city", true];
+const city = ["Man united", true];
+const hotspur = ["Livepool", true];
+//meaning it expects two or more tuples of type Matches followed by a string
+function processMatches(...matches) {
+    const lastMatch = matches.pop();
+    console.log("previous matches:");
+    for (const match of matches) {
+        console.log(match[0]);
+    }
+    console.log("last match:", lastMatch);
+}
+processMatches(arsenal, city, hotspur, "Chelsea vs. Arsenal");
 //--------------
 // named tuples
 //--------------
 let user;
-user = ['peach', 25];
+user = ["peach", 25];
 console.log(user[0]);
--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,61 @@
 // tuples
 //--------
 
-let person: [string, number, boolean] = ['mario', 30, true]
+let person: [string, number, boolean] = ["mario", 30, true];
 
 // person[0] = false
-person[0] = 'luigi'
+person[0] = "luigi";
+console.log(person[0]);
 
 //-----------------
 // tuples examples
 //-----------------
 
-let hsla: [number, string, string, number]
-hsla = [200, '100%', '50%', 1]
+let hsla: [number, string, string, number];
+hsla = [200, "100%", "50%", 1];
 
-let xy: [number, number]
-xy = [94.7, 20.1]
+let xy: [number, number];
+xy = [94.7, 20.1];
+
+let tup: [string, number];
+tup = ["Alex", 5];
 
 function useCoords(): [number, number] {
-	// get coords of something
+  // get coords of something
 
-	const lat = 100
-	const long = 50
+  const lat = 100;
+  const long = 50;
 
-	return [lat, long]
+  return [lat, long];
 }
 
-const [lat, long] = useCoords()
+const [lat, long] = useCoords();
 
+// Rest parameters
+declare function example(...args: [string, number]): void;
+declare function example(args0: string, args1: number): void;
+
+type Matches = [string, boolean];
+const arsenal: Matches = ["Man city", true];
+const city: Matches = ["Man united", true];
+const hotspur: Matches = ["Livepool", true];
+//meaning it expects two or more tuples of type Matches followed by a string
+function processMatches(...matches: [...Matches[], string]): void {
+  const lastMatch = matches.pop();
+  console.log("previous matches:");
+
+  for (const match of matches) {
+    console.log(match[0]);
+  }
+  console.log("last match:", lastMatch);
+}
+
+processMatches(arsenal, city, hotspur, "Chelsea vs. Arsenal");
 //--------------
 // named tuples
 //--------------
 
-let user: [name: string, age: number]
+let user: [name: string, age: number];
 
-user = ['peach', 25]
-console.log(user[0])
+user = ["peach", 25];
+console.log(user[0]);
