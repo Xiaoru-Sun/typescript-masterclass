85280d7c550d035d3bb98c2f82645d696fe31971
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-14 11:26:24 +0100

    adds another use case for interface

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,13 @@
 //------------
 // interfaces
 //------------
-const authorOne = { name: 'mario', avatar: '/img/mario.png' };
+const authorOne = { name: "mario", avatar: "/img/mario.png" };
+const authorTwo = { name: "lily", avatar: "/img/lily.png" };
+// newPost object does not have to be typed
 const newPost = {
-    title: 'my first post',
-    body: 'something interesting',
-    tags: ['gaming', 'tech'],
+    title: "my first post",
+    body: "something interesting",
+    tags: ["gaming", "tech"],
     created_at: new Date(),
     author: authorOne,
 };
@@ -22,5 +24,18 @@ createPost(newPost);
 // with arrays
 //-------------
 let posts = [];
+console.log("posts", posts);
 // posts.push({ title: 'some title' })
 posts.push(newPost);
+function calculateArea(shape) {
+    console.log(`calculating area of ${shape.name}`);
+    console.log(`Area:${shape.area()}`);
+}
+const circle = {
+    name: "Circle",
+    color: "Red",
+    area() {
+        return Math.PI * 2 * 2;
+    },
+};
+calculateArea(circle);
--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,68 @@
 //------------
 
 interface Author {
-	name: string
-	avatar: string
+  name: string;
+  avatar: string;
 }
 
-const authorOne: Author = { name: 'mario', avatar: '/img/mario.png' }
+const authorOne: Author = { name: "mario", avatar: "/img/mario.png" };
+const authorTwo: Author = { name: "lily", avatar: "/img/lily.png" };
 
 interface Post {
-	title: string
-	body: string
-	tags: string[]
-	created_at: Date
-	author: Author
+  title: string;
+  body: string;
+  tags: string[];
+  created_at: Date;
+  author: Author;
 }
 
-const newPost = {
-	title: 'my first post',
-	body: 'something interesting',
-	tags: ['gaming', 'tech'],
-	created_at: new Date(),
-	author: authorOne,
-}
+// newPost object does not have to be typed
+const newPost: Post = {
+  title: "my first post",
+  body: "something interesting",
+  tags: ["gaming", "tech"],
+  created_at: new Date(),
+  author: authorOne,
+};
 
 //----------------------------
 // as function argument types
 //----------------------------
 
 function createPost(post: Post): void {
-	console.log(`created post ${post.title} by ${post.author.name}`)
+  console.log(`created post ${post.title} by ${post.author.name}`);
 }
 
 // createPost({ title: 'a new post title' })
-createPost(newPost)
+createPost(newPost);
 
 //-------------
 // with arrays
 //-------------
 
-let posts: Post[] = []
+let posts: Post[] = [];
+console.log("posts", posts);
 
 // posts.push({ title: 'some title' })
-posts.push(newPost)
+posts.push(newPost);
+
+// more example
+interface Shape {
+  name: string;
+  color: string;
+  area(): number;
+}
+
+function calculateArea(shape: Shape): void {
+  console.log(`calculating area of ${shape.name}`);
+  console.log(`Area:${shape.area()}`);
+}
+const circle: Shape = {
+  name: "Circle",
+  color: "Red",
+  area() {
+    return Math.PI * 2 * 2;
+  },
+};
+
+calculateArea(circle);
